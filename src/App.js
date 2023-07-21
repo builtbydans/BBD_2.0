098e4bbef6a5776a252f425b235e0b84de94a3ea
Danish Shafi <danish.shafi@live.co.uk>
2023-07-21 21:39:49 +0100

    Testing fonts, spacing and sizing according to Figma file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import './assets/css/fonts.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>BBD</h3>
+      <h1>Product Manager & Software Engineer</h1>
+      <h2>Building human-viable products;<br/>from ideation to codebase.</h2>
     </div>
   );
 }
